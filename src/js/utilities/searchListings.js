f9039da88bf200cedd145c463b9acb8ac4e82d07
h-bcode <jon.are.br@gmail.com>
2024-09-04 10:16:53 +0200

    search functionallity is good

--- src/js/utilities/searchListings.js
+++ src/js/utilities/searchListings.js
@@ -1,19 +1,30 @@
 export async function searchListings() {
   const formElement = document.getElementById('searchForm');
-
   const inputElement = document.getElementById('searchInput');
+
+  // I wanted to have search-function with key up, but then I had disabled the submit event with preventDefault.
 
   if (formElement && inputElement) {
     formElement.addEventListener('submit', function (event) {
       event.preventDefault();
+    });
 
+    inputElement.addEventListener('keyup', function () {
       const value = inputElement.value.toLowerCase();
 
-      const containers = document.getElementById('listings');
+      const listingsContainer = document.getElementById('listings');
+      const listingsCards = listingsContainer.querySelectorAll('.card');
 
-      containers.forEach(function (container) {
-        const text = container.textContent.toLowerCase();
-        container.style.display = text.includes(value) ? '' : 'none';
+      listingsCards.forEach(function (item) {
+        const text = item.textContent.toLowerCase();
+
+        if (value === '') {
+          item.style.display = '';
+        } else if (text.includes(value)) {
+          item.style.display = '';
+        } else {
+          item.style.display = 'none';
+        }
       });
     });
   }
