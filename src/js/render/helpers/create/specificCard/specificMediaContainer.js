37c5b554c07e7ed81ee47a60a5f17694e09ff40c
h-bcode <jon.are.br@gmail.com>
2024-09-10 14:47:45 +0200

    tried to make specific card align in the center of the screen. still got scrolling vertically

--- src/js/render/helpers/create/specificCard/specificMediaContainer.js
+++ src/js/render/helpers/create/specificCard/specificMediaContainer.js
@@ -1,6 +1,6 @@
 export function specificMediaContainer(postData) {
   const mediaContainer = document.createElement('div');
-  mediaContainer.classList.add('col-12', 'col-lg-12', 'mb-3', 'mb-lg-0'); // mb-3 for space on mobile, no space on larger screens
+  mediaContainer.classList.add('col-12', 'col-lg-6', 'mb-3', 'mb-lg-0'); // mb-3 for space on mobile, no space on larger screens
 
   const img = document.createElement('img');
   img.classList.add('img-fluid', 'rounded', 'object-fit-cover');
