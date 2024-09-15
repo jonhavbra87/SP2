3a3dbd9930b02d47694afeb868abfed2c0735ac9
h-bcode <jon.are.br@gmail.com>
2024-09-15 15:48:35 +0200

    updated carousel styling

--- src/js/render/renderCarousel.js
+++ src/js/render/renderCarousel.js
@@ -1,4 +1,6 @@
 import { getListings } from '../api/fetch/getListings';
+import { hideLoader } from '../ui/helpers/hideLoader';
+import { showLoader } from '../ui/helpers/showLoader';
 import { initializeCarousel } from '../utilities/initializeCarousel';
 import { shuffleListings } from '../utilities/shuffleListings';
 import { createCarousel } from './helpers/create/crateCarousel';
@@ -7,12 +9,13 @@ export async function renderCarousel() {
   const limit = 6;
   const container = document.querySelector('.slider');
   container.innerHTML = '';
+  showLoader();
 
   try {
     let listings = await getListings();
 
     listings = shuffleListings(listings);
-
+    hideLoader();
     listings.slice(0, limit).forEach((postData) => {
       createCarousel(postData);
     });
