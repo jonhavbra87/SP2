91a8e0d7a565da417c13105730ad3e1d77f3ad92
h-bcode <jon.are.br@gmail.com>
2024-09-17 11:55:38 +0200

    fixed loader is null in renderCarousel

--- src/js/render/renderCarousel.js
+++ src/js/render/renderCarousel.js
@@ -1,6 +1,4 @@
 import { getListings } from '../api/fetch/getListings';
-import { hideLoader } from '../ui/helpers/hideLoader';
-import { showLoader } from '../ui/helpers/showLoader';
 import { initializeCarousel } from '../utilities/initializeCarousel';
 import { shuffleListings } from '../utilities/shuffleListings';
 import { createCarousel } from './helpers/create/crateCarousel';
@@ -9,13 +7,12 @@ export async function renderCarousel() {
   const limit = 6;
   const container = document.querySelector('.slider');
   container.innerHTML = '';
-  showLoader();
 
   try {
     let listings = await getListings();
 
     listings = shuffleListings(listings);
-    hideLoader();
+
     listings.slice(0, limit).forEach((postData) => {
       createCarousel(postData);
     });
