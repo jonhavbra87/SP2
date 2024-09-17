ae74067ed9415a8405398d671dd854f0808645bb
h-bcode <jon.are.br@gmail.com>
2024-09-17 16:22:57 +0200

    added d-none to nac-link

--- src/js/ui/helpers/updateNavbarGuestUser.js
+++ src/js/ui/helpers/updateNavbarGuestUser.js
@@ -11,10 +11,10 @@ export function updateNavbarGuestUser() {
   if (loginButton) loginButton.classList.remove('d-none');
   if (registerButton) registerButton.classList.remove('d-none');
 
-  // Hide logged-in navbar items
-  // if (homeLink) homeLink.classList.add('d-none');
-  // if (profileLink) profileLink.classList.add('d-none');
-  // if (listingsLink) listingsLink.classList.add('d-none');
-  // if (newListingLink) newListingLink.classList.add('d-none');
-  // if (logoutButton) logoutButton.classList.add('d-none');
+  //Hide logged-in navbar items
+  if (homeLink) homeLink.classList.add('d-none');
+  if (profileLink) profileLink.classList.add('d-none');
+  if (listingsLink) listingsLink.classList.add('d-none');
+  if (newListingLink) newListingLink.classList.add('d-none');
+  if (logoutButton) logoutButton.classList.add('d-none');
 }
